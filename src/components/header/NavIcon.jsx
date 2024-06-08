73acd265be7343d8b30385470e016b71e59e70c8
LscWFr713 <LscWFr713@outlook.com>
2024-06-08 05:14:22 +0000

    Cog Icon Oculted

--- src/components/header/NavIcon.jsx
+++ src/components/header/NavIcon.jsx
@@ -3,7 +3,7 @@ export default function NavIcon({darkMode, setDarkMode}) {
         <section className="navIcon">
             <a href="https://github.com/Lusasaja23/mgcodes" target="_blank" rel="noopener noreferrer"><i className="bx bxl-github"></i></a>
             <i className={`bx bx-${darkMode ? 'moon' : 'sun'}`} onClick={() => { darkMode ? setDarkMode(false) : setDarkMode(true) }}></i>
-            <i className="bx bx-cog"></i>
+            <i className="bx bx-cog" id="cogIcon" title="Próximamente..."></i>
         </section>
     )
 }
